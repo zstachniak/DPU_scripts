1eaf26f3cb761785d4a92f143793426f91a98830
Zander Stachniak <zstachniak@gmail.com>
2018-01-12 16:54:37 -0600

    Add batch file creation function

--- dpu/scripts.py
+++ dpu/scripts.py
@@ -5,5 +5,185 @@ Created on Fri Jan 12 15:16:16 2018
 @author: astachn1
 """
 
-def hello ():
-    print('hello world')
+import os
+import pandas as pd
+from datetime import datetime
+from fuzzywuzzy import fuzz, process
+
+#############################################################
+# Functions
+#############################################################
+
+def get_latest (pathname, filename, **kwargs):
+    '''Scans the pathname folder for all files that match the typical 
+    naming conventions of "filename". For those files, function parses 
+    file name to look for a suffix to indicate date edited, then returns
+    the file name with the latest date.
+    
+    @ Optional Keyword Arguments:
+    ----------------------
+    date_format: indicate a date format for files (default: '%Y-%m-%d')
+    num_files: number of files to return (if more than one, in ordered list)
+    ignore_suffix: an additional file suffix to ignore
+    '''
+    # Gather optional keyword arguments
+    date_format = kwargs.pop('date_format', '%Y-%m-%d')
+    num_files= kwargs.pop('num_files', 1)
+    ignore_suffix = kwargs.pop('ignore_suffix', None)    
+    #Set up storage dict
+    file_dict = {}
+    # Iterate through objects in directory
+    for name in os.listdir(pathname):
+        # Concatenate the path with the object name
+        subname = os.path.join(pathname, name)
+        #Checks for standard naming conventions and ignores file fragments
+        if os.path.isfile(subname) and filename in subname and '~$' not in subname:
+            # Split into name and extension
+            f_name, ext = os.path.splitext(subname)
+            if ignore_suffix:
+                # Ignore files that end in user-identified suffix
+                if f_name.endswith(ignore_suffix):
+                    continue
+            else:
+                # Determine the expected string length of date
+                date_length = len(datetime.now().strftime(date_format))                    
+                # Gather expected date portion
+                date_suffix = f_name[-date_length:]
+                # Attempt to convert to datetime and add to file_dict
+                try:
+                    date_time = datetime.strptime(date_suffix, date_format).date()
+                    file_dict[date_time] = subname
+                except:
+                    print('{} does not meet expected date convention and has been skipped.'.format(f_name))         
+    # If no files found, exit function
+    if len(file_dict) == 0:
+        return 'No files were found'
+    # Gather the most recent date in dictionary keys
+    latest = max(list(file_dict.keys()))
+    # If only one file is requested OR there is only one file found, return
+    if num_files == 1 or len(file_dict) == 1:
+        return file_dict[latest]
+    else:
+        file_list = [file_dict.pop(latest)]
+        while len(file_list) < num_files and len(file_dict) != 0:
+            # Gather the new latest file
+            latest = max(list(file_dict.keys()))
+            # Pop the latest file (i.e., remove from dictionary)
+            file_list.append(file_dict.pop(latest))
+        return file_list
+    
+def guess_current_term ():
+    '''A function that uses the current date to estimate what the current
+    academic term should be. Assumes the following cross-over points:
+    12/1 - winter
+    3/25 - spring
+    6/10 - summer
+    9/1 - fall
+    '''
+    # Get term descriptions
+    TermDescriptions = pd.read_excel('W:\\csh\\Nursing\\Schedules\\Term Descriptions.xlsx', header=0, converters={'Term':str})
+    # Get the current date and year
+    today = datetime.today()
+    current_year = today.year
+    # Determine which cross-over points have been reached to assign quarter
+    # For end of year dates, we increment year by one to reach correct ay
+    if today > datetime(current_year, 12, 1):
+        quarter = 'Winter'
+        current_year += 1
+    elif today > datetime(current_year, 9, 1):
+        quarter = 'Autumn'
+        current_year += 1
+    elif today > datetime(current_year, 6, 10):
+        quarter = 'Summer'
+    elif today > datetime(current_year, 3, 25):
+        quarter = 'Spring'
+    else:
+        quarter = 'Winter'
+    # Assign academic year from current year + quarter
+    ay = f'{current_year - 1}-{current_year}'
+    # Get current term from TermDescriptions
+    term = TermDescriptions[(TermDescriptions['Academic Year'] == ay) & (TermDescriptions['Quarter'] == quarter)]['Term'].item()
+    return term
+
+def get_cln (starting_path, term):
+    '''A function to gather clinical rosters from path & term.'''
+    # Get term descriptions
+    TermDescriptions = pd.read_excel('W:\\csh\\Nursing\\Schedules\\Term Descriptions.xlsx', header=0, converters={'Term':str})
+    # Gather academic year and quarter
+    ay = TermDescriptions[TermDescriptions['Term'] == term]['Academic Year'].item()
+    q = TermDescriptions[TermDescriptions['Term'] == term]['Quarter'].item()
+    # Update file path
+    folder = os.path.join(starting_path, ay, q)
+    file = get_latest(folder, 'Clinical Roster')
+    # Read data
+    clinical_roster = pd.read_excel(file, header=0, converters={'Term':str, 'Cr':str, 'Student ID':str})
+    return clinical_roster
+
+def get_schedule (starting_path, term):
+    '''A function to gather a quarterly schedule from path & term.'''
+    # Get term descriptions
+    TermDescriptions = pd.read_excel('W:\\csh\\Nursing\\Schedules\\Term Descriptions.xlsx', header=0, converters={'Term':str})
+    # Gather quarter
+    q = TermDescriptions[TermDescriptions['Term'] == term]['Quarter'].item()
+    # If Summer, increment term to account for fiscal year changeover
+    if q == 'Summer':
+        term = str(int(term) + 5)
+    # Gather academic year
+    ay = TermDescriptions[TermDescriptions['Term'] == term]['Academic Year'].item()
+    # Update file path
+    folder = os.path.join(starting_path, ay)
+    file = get_latest(folder, 'Fiscal Schedule', date_format='%m-%d-%Y')
+    # Read data
+    schedule = pd.read_excel(file, sheet_name=q, header=0,converters={'Cr':str, 'Term':str})
+    return schedule
+
+def find_best_string_match (query, choices, **kwargs):
+    '''This function takes a single query and a list of possible
+    choices and ranks the choices to find the most likely match.
+    Rankings are calculated via fuzzywuzzy ratios, and can be
+    passed directly by the user via optional keyword.'''
+    # Optional argument to test only certain scorers
+    scorers = kwargs.pop('scorers', [fuzz.ratio, fuzz.partial_ratio, fuzz.token_sort_ratio, fuzz.token_set_ratio])
+    # Initialize a dictionary to store scoring
+    score_mapping = {}
+    for key in choices:
+        score_mapping[key] = []
+    # Test for each scorer
+    for scorer in scorers:
+        # Store temporary results as list of tuples
+        temp_results = process.extract(query, choices, scorer=scorer, limit=None)
+        # Add scores to mapping
+        for (key, score) in temp_results:
+            score_mapping[key].append(score)
+    # Sum all results for each key
+    for key in score_mapping.keys():
+        score_mapping[key] = sum(score_mapping[key])
+    # Determine the maximum scored
+    result = max(score_mapping, key=lambda key: score_mapping[key])
+    return result
+
+def build_File (pathname, tempdict, file_format='csv'):
+    '''Iterates recursively through a folder and combines files into a
+    dictionary pair of filename (key) and dataframe (value). Works with
+    csv and excel formats.'''
+    # Iterate through objects in pathname folder
+    #print('Scanning: {}'.format(pathname))
+    for name in os.listdir(pathname):
+        subname = os.path.join(pathname, name)
+        # If object in folder is a file
+        if os.path.isfile(subname):
+            #print('Adding: {}'.format(subname))
+            # Path naming
+            base = os.path.basename(subname)
+            dfname = os.path.splitext(base)[0]
+            # Read file using user-defined format
+            if file_format == 'csv':
+                tempdict[dfname] = pd.read_csv(subname,delimiter=',',na_values='nan',)
+            elif file_format == 'excel':
+                tempdict[dfname] = pd.read_excel(subname,skiprows=0,header=1,na_values='nan')
+        # If object in folder is a folder, recursive call to function
+        elif os.path.isdir(subname):
+            build_File(subname, tempdict)
+        else:
+            pass
+    return tempdict
