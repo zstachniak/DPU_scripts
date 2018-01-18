a751a54cf283925e4e240a53b415f36ba060b414
Zander Stachniak <zstachniak@gmail.com>
2018-01-18 10:53:34 -0600

    Fixed output issue.

--- clinical_review.py
+++ clinical_review.py
@@ -7,34 +7,18 @@ Created on Thu Jan 18 09:42:38 2018
 
 import dpu.scripts as dpu
 import click
+import os
+import pandas as pd
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def filter_schedule_for_cln (df, program_list):
+    '''Filters schedule to only include clinical courses within a program.'''
+    # Gather list of clinical courses
+    cln_list = df[(df['Program'].isin(program_list)) & (df['Type'] == 'PRA')]['Cr'].unique().tolist()
+    # Add lonely 301
+    cln_list.append('301')
+    # Keep only clinical courses
+    df = df[(df['Program'].isin(program_list)) & (df['Cr'].isin(cln_list)) & (~df['Type'].isin(['LEC', 'COORD']))].copy(deep=True)
+    return df
 
 #############################################################
 # Main
@@ -60,9 +44,12 @@ def main (term, campus):
     
     # If a campus is not passed, use both
     if not campus:
-        campus = ['LPC', 'RFU']
-    # Convert string to list so code execution can be parallel
+        campus = ['MENP', 'RFU']
     else:
+        # In schedule, 'LPC' is represented by 'MENP'
+        if campus == 'LPC':
+            campus = 'MENP'
+        # Convert string to list so code execution can be parallel
         campus = [campus]
     
     # Define schedule starting path
@@ -71,8 +58,20 @@ def main (term, campus):
     # Get schedule
     schedule = dpu.get_schedule(path, term)
     
+    # Filter out non-clinical courses
+    schedule = filter_schedule_for_cln(schedule, campus)
     
+    # Reorder and filter out unncessary data
+    schedule = schedule[['Cr', 'Sec', 'Time', 'Clinical Site', 'Unit', 'Max Cap', 'Confirmed', 'Faculty']]
+        
+    # Get output folder
+    output_folder = os.path.join(dpu.get_dir_of_schedule(term), 'Charts')
     
+    # Output file name
+    output_file = os.path.join(output_folder, 'Clinical Review {}.xlsx'.format(term))
+    
+    # Output result
+    schedule.to_excel(output_file, index=False)
 
 if __name__ == "__main__":
     main()
